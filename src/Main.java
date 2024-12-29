12b6a27f624972f1fbc10fc4b089ee34297a8a99
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-12-29 16:56:01 +0200

    adding welcome ALI

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
-
+//ALI
     public static void main (String [] arg){
 
         System.out.println("Welcome To Print it Application");
         System.out.println("Hello From Saied");
+        System.out.println("Hello From ALI ");
     }
 }
