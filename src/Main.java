dc2da25cc3a97d864e7a7c4e3246c47606109a0b
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-12-29 16:48:14 +0200

    adding welcome msg from saied

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main (String [] arg){
 
         System.out.println("Welcome To Print it Application");
+        System.out.println("Hello From Saied");
     }
 }
